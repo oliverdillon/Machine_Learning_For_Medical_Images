c6ad46a74815d39e2792e77e9944e59516de5ae4
oliverdillon <45541944+oliverdillon@users.noreply.github.com>
2020-03-22 16:32:20 +0000

    Update Image_Data_Generatorv2.py
    Normilse Data

--- Image_Data_Generatorv2.py
+++ Image_Data_Generatorv2.py
@@ -39,6 +39,8 @@ class Data_Generator_3D(Sequence):
     def Get_Input(self,filename):
         img = np.load(filename)
         print(filename)
+        img = img[...,1]
+        img = np.divide(img,255)
         return img
 
     def Get_Output(self,filename):    
