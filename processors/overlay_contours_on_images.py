87f4bf6f08e874625cd51d16d314650ce88dcca2
oliverdillon <45541944+oliverdillon@users.noreply.github.com>
2021-09-21 22:15:02 +0100

    Match ct images and contour data

--- processors/overlay_contours_on_images.py
+++ processors/overlay_contours_on_images.py
@@ -1,20 +1,38 @@
 class Overlay_contours_on_images:
     def __init__(self,dataset):
+        self.filter_date =""
         self.dataset = dataset
-        self.filter_patients_by_modality()
+        self.filter_patient_data()
 
-    def is_required_data(self,series):
-        if series.modality =="CT":
-            return True
-        elif series.modality =="RTSTRUCT":
+
+    def is_contour_data(self,series):
+        if series.modality =="RTSTRUCT":
             return True
         return False
 
-    def filter_patients_by_modality(self):
+    def is_image_data(self,series):
+        if series.modality =="CT" and series.study_date == self.filter_date:
+            return True
+        return False
+
+    def get_contour_data(self,patient):
+        series_filtered =[]
+        series_iterator = filter(self.is_contour_data,patient.series)
+        for series in series_iterator:
+            series_filtered.append(series)
+            self.filter_date = series.study_date;
+        # assert(series_filtered,len(1))
+        return series_filtered
+
+    def add_ct_images(self,patient,series_filtered):
+        series_iterator = filter(self.is_image_data, patient.series)
+        for series in series_iterator:
+            series_filtered.append(series)
+
+    def filter_patient_data(self):
         data = self.dataset.data
         for patient in data:
-            series_filtered = []
-            series_iterator = filter(self.is_required_data,patient.series)
-            for series in series_iterator:
-                series_filtered.append(series)
+            series_filtered = self.get_contour_data(patient)
+            self.add_ct_images(patient,series_filtered)
+
             patient.series = series_filtered
