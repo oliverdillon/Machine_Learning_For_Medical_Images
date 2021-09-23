a6247a1e906b773afe47fd937d7f4f9a42644ed8
oliverdillon <45541944+oliverdillon@users.noreply.github.com>
2021-09-23 22:27:25 +0100

    Add overlay for brainstem

--- models/ctimage.py
+++ models/ctimage.py
@@ -1,8 +1,9 @@
-# from dicompylercore import dicomparser
+from dicompylercore import dicomparser
 import pydicom
 class CTImage:
     def __init__(self,image_dir):
         self.pydicom= pydicom.read_file(image_dir)
+        self.dicomparser= dicomparser.DicomParser(image_dir)
         self.zlocation = self.pydicom.ImagePositionPatient[2]
         # self.dicomparser = dicomparser.DicomParser(image_dir)
 
--- processors/overlay_contours_on_images.py
+++ processors/overlay_contours_on_images.py
@@ -1,9 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
 class Overlay_contours_on_images:
     def __init__(self,dataset):
         self.filter_date =""
         self.dataset = dataset
         self.filter_patient_data()
-
+        self.overlay_contours()
 
     def is_contour_data(self,series):
         if series.modality =="RTSTRUCT":
@@ -36,3 +38,34 @@ class Overlay_contours_on_images:
             self.add_ct_images(patient,series_filtered)
 
             patient.series = series_filtered
+
+    def overlay_contours(self):
+        data = self.dataset.data
+        for patient in data:
+            Organ_Dictionary = patient.series[0].contours_data
+            Organ_Map = patient.series[0].organs;
+            ct_images = patient.series[1].medical_images
+
+            for key, value in Organ_Map.items():
+                if value == "Brainstem":
+                    Organ_Data = Organ_Dictionary[key]
+
+                    for ct_image in ct_images:
+                        # ImageArray = ct_image.pydicom.pixel_array
+                        image = ct_image.dicomparser.GetImage(300,40)
+                        image = image.convert(mode ='RGB')
+                        ImageArray = np.asarray(image)
+                        ImageArray.flags.writeable = 1
+                        # plt.imshow(ImageArray)
+                        # plt.title("Test")
+                        # plt.show()
+                        #Loops through contour data
+                        for i in range(0,len(Organ_Data)-1):
+                            for j in range(0,len(Organ_Data[i])-1):
+                                    OrganIndex1 = int((Organ_Data[i][j][0]-ct_image.pydicom.ImagePositionPatient[0])/ct_image.pydicom.PixelSpacing[0])
+                                    OrganIndex2 = int((Organ_Data[i][j][1]-ct_image.pydicom.ImagePositionPatient[1])/ct_image.pydicom.PixelSpacing[1])
+                                    ImageArray[OrganIndex2][OrganIndex1][2] = 255;
+
+                        # plt.imshow(ImageArray)
+                        # plt.title("Test")
+                        # plt.show()
