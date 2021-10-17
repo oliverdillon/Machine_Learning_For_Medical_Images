9a9b242c10e93af8d6fe9f353c94abe365162884
oliverdillon <45541944+oliverdillon@users.noreply.github.com>
2021-10-17 20:38:04 +0100

    directory injection

--- processors/batch_save_feature_files.py
+++ processors/batch_save_feature_files.py
@@ -2,10 +2,11 @@ from processors.process_and_save_feature_files import Process_and_save_feature_f
 from processors.extract_dataset import Extract_dataset
 import os
 class Batch_save_feature_files:
-    def __init__(self):
+    def __init__(self,directory):
         self.patient_batch_size= 2
+        self.directory = directory
         self.extract_save_files()
 
     def extract_save_files(self):
-        extract_dataset = Extract_dataset(os.getcwd()+"/metadata.csv", self.patient_batch_size)
+        extract_dataset = Extract_dataset(self.directory, self.patient_batch_size)
         filtered_dataset = Process_and_save_feature_files(extract_dataset.dataset)
