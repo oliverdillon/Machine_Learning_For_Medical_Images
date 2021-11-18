8b7dc6972fd9f1d3f8b70c023566241dbc0fcf88
oliverdillon <45541944+oliverdillon@users.noreply.github.com>
2021-11-18 13:30:09 +0000

    plot images for a patient as a gif

--- processors/plot_medical_images.py
+++ processors/plot_medical_images.py
@@ -3,6 +3,9 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+
+from matplotlib import animation
+
 class Plot_medical_images:
     def __init__(self, directory, allowed_organ):
         self.image_width = self.image_height = 512
@@ -28,6 +31,8 @@ class Plot_medical_images:
                 if patient_no_searcher and organ_no_searcher:
                     patient_no = patient_no_searcher.group(1)
                     organ = organ_no_searcher.group(1)
+                    self.create_ct_image_video(image, organ, 'HNSCC-01-'+patient_no)
+
                     if self.allowed_organs.index(organ) == 1:
                         self.patient_class_1.append(patient_no)
                         self.plot_ct_image(image, self.class_1)
@@ -52,3 +57,20 @@ class Plot_medical_images:
         plt.imshow(overlay_sagittal)
         plt.close()
         class_no.append(total_contour_size)
+
+    def create_ct_image_video(self, ct_image_3d, organ, patient_name):
+        #https://matplotlib.org/2.1.2/gallery/animation/dynamic_image2.html
+        fig1 = plt.figure()
+        plt.title(patient_name+"_"+organ)
+        ims = []
+        for i in range(len(ct_image_3d)):
+            im = plt.imshow(ct_image_3d[i], animated=True)
+            ims.append([im])
+
+        ani = animation.ArtistAnimation(fig1, ims, interval=200, blit=True,
+                                        repeat_delay=1000)
+        print(self.base_directory + "/"+patient_name + "/" + organ + ".gif")
+        ani.save(self.base_directory + "/"+patient_name + "/" + organ + ".gif", dpi=480)
+        plt.close()
+
+
